a9867c55ea8ffee5305c47f64efe5a463a72a065
christian <chro@itu.dk>
2013-04-04 08:42:49 +0200

    newest changes to Arduino added

--- Arduino/TCOSerialTestRead/TCOSerialTestRead.ino
+++ Arduino/TCOSerialTestRead/TCOSerialTestRead.ino
@@ -9,7 +9,7 @@ void setup(){
 void loop(){
   if(tcoSerial.readSerial()){
     // set global state
-    int i = 0;
+    byte i = 0;
     while (i < tcoSerial.readIndex){ // TODO: Write logic for signals
       Serial.write(tcoSerial.readQueue[i++]);
     }
