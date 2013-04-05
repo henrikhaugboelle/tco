f0f2f4f2d4261d35b68e906da7f8cb2dd84325de
Henrik Haugbølle <henrikhaugboelle@live.dk>
2013-04-05 15:11:57 +0200

    simple prototype signal converter

---
+++ experiment/converter-average.js
@@ -0,0 +1,28 @@
+var AverageConverter = _.inherit(Converter, {
+	time: 100,
+	items: 0,
+
+	calculate: function() {
+		var temp = [];
+
+		var number_of_sets = this.value_sets.length;
+
+		temp = this.value_sets.pop();
+		while (this.value_sets.length > 0) {
+			var value_set = this.value_sets.pop();
+
+			for (var i in value_set) {
+				temp[i] = parseInt(temp[i] || 0) + parseInt(value_set[i]);
+			}
+		}
+
+		for (var i in temp) {
+			temp[i] = Math.round(temp[i] / number_of_sets);
+		}
+
+		this.invoke(temp || []);
+	}
+
+});
+
+if (typeof module != 'undefined' && module.exports) module.exports = AverageConverter;
